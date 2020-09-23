12a985be26564f192a0d3a10880261d08afdcad0
rishabh <rishabhc329@gmail.com>
2020-09-23 12:01:44 +0530

    Updates

---
+++ precache-manifest.7262d4d774868f2faa60b502c88e02a4.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c25997d176f0d55eaa196a72e2b0e85",
+    "url": "/index.html"
+  },
+  {
+    "revision": "65051aa86ff8499a4447",
+    "url": "/static/css/2.f1878fa8.chunk.css"
+  },
+  {
+    "revision": "907997cf95ffd083fe81",
+    "url": "/static/css/main.725b2dad.chunk.css"
+  },
+  {
+    "revision": "65051aa86ff8499a4447",
+    "url": "/static/js/2.3b3ccab2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.3b3ccab2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "907997cf95ffd083fe81",
+    "url": "/static/js/main.2fbf94b9.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "923d38d23b1eac06fa0070ea434e02ef",
+    "url": "/static/media/Github.923d38d2.svg"
+  },
+  {
+    "revision": "958a4c9f155b6032f2b1adae9b1cea67",
+    "url": "/static/media/Gmail.958a4c9f.svg"
+  },
+  {
+    "revision": "3df0e592055cf04b438d046e62742c1a",
+    "url": "/static/media/Linkedin.3df0e592.svg"
+  },
+  {
+    "revision": "d8d292dae227adf8fadb5b4272325336",
+    "url": "/static/media/background.d8d292da.png"
+  },
+  {
+    "revision": "328561d945372a4811cceca9996fbb0d",
+    "url": "/static/media/home-button-arrow.328561d9.svg"
+  }
+]);
